d15ad3ad1ea62033574ba81a7bb1e1c465068723
Joao <joao.pagano01@gmail.com>
2026-09-22 18:01:38 -0300

    adicionar firebase

--- loginFirebase/config/firebase.js
+++ loginFirebase/config/firebase.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAgySc6-usjxj1kpxAzz-SFC33uFecXD-Q",
-  authDomain: "login-firebase-23455.firebaseapp.com",
-  projectId: "login-firebase-23455",
-  storageBucket: "login-firebase-23455.firebasestorage.app",
-  messagingSenderId: "654632770360",
-  appId: "1:654632770360:web:0743cc675b0f7ea36b7fcd",
-  measurementId: "G-6KBCTBZ24W"
+  apiKey: "AIzaSyBOJ6hG0TDmI7-ic9HAKVZTV7YvBVF9dZI",
+  authDomain: "fir-attpam.firebaseapp.com",
+  projectId: "fir-attpam",
+  storageBucket: "fir-attpam.firebasestorage.app",
+  messagingSenderId: "60272664242",
+  appId: "1:60272664242:web:51459ad738d6888eb5f3ab",
+  measurementId: "G-Q75XSXX9SZ"
 };
 
 // Initialize Firebase
